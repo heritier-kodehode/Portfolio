48c674a8a36bcb5130573db7102df08d5472e2cb
heritier-kodehode <106584001+heritier-kodehode@users.noreply.github.com>
2024-01-27 16:31:32 +0100

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,14 +11,12 @@ export const langDataEng = {
   aboutIntrotext: [
     {
       text: 'I am a Full-Stack Developer with a focus on HTML, CSS, JavaScript, React, and implementation of new technologies that can complement the tech stack in use. I can be contacted via email at heritier.kodehode@gmail.com.',
-      text2:
-        'Currently, I works as a Front-End Developer at Medretur since 2022, where My responsibilities include planning and holding meetings, developing UX/UI with React, and backend with Firebase and other CMS systems.',
-      text3:
-        'From 2022 to 2022, I worked as a Front-End Developer for Jobloop, where I developed a Planetarium with a team of three. I also developed the codebase with Github branches, set up the development environment, and developed and maintained a chat app.',
-      text4:
-        'From 2017 to 2022, I worked as a Freelance Front-End Developer. My responsibilities included fixing bugs on the front-end, implementing paywalls with Stripe, holding meetings with the front-end team to explore weaknesses in the system, inspecting code for refactoring and optimization.',
+      text2: 'Currently, I work as a Front-End Developer at Medretur since 2022, where my responsibilities include planning and holding meetings, developing UX/UI with React, and backend with Firebase and other CMS systems.',
+      text3: 'From 2022 to 2022, I worked as a Front-End Developer for Jobloop, where I developed a Planetarium with a team of three. I also developed the codebase with Github branches, set up the development environment, and developed and maintained a chat app.',
+      text4: 'From 2017 to 2022, I worked as a Freelance Front-End Developer. My responsibilities included fixing bugs on the front-end, implementing paywalls with Stripe, holding meetings with the front-end team to explore weaknesses in the system, inspecting code for refactoring and optimization.',
     },
   ],
+
   projectText: [{ text: 'Visit site' }, { text: 'See Code' }],
   buttonText: [{ text: 'More Projects' }, { text: 'Contact Me' }],
   sectionTitle: [
@@ -54,15 +52,13 @@ export const langDataNok = {
   ],
   aboutIntrotext: [
     {
-      text: 'Jeg er en Full-Stack-utvikler med fokus på HTML, CSS, JavaScript, React og implementering av nye teknologier som kan utfylle teknologistabelen som er i bruk. Jeg kan kontaktes via e-post på heritier.kodehode@gmail.com.',
-      text2:
-        'For tiden jobber jeg som front-end-utvikler hos Medretur siden 2022, hvor hans ansvar inkluderer planlegging og avholdelse av møter, utvikling av UX/UI med React og backend med Firebase og andre CMS-systemer.',
-      text3:
-        'Fra 2022 til 2022 jobbet jeg som front-end-utvikler for Jobloop, hvor han utviklet et planetarium med et team på tre. Han utviklet også kodebasen med Github-grener, satte opp utviklingsmiljøet og utviklet og vedlikeholdt en chat-app.',
-      text4:
-        'Fra 2017 til 2022 jobbet jeg som freelance front-end-utvikler. Hans ansvar inkluderte å fikse feil på front-end, implementere betalingsmurer med Stripe, holde møter med front-end-teamet for å utforske svakheter i systemet, inspisere kode for refaktorisering og optimalisering.',
+      text: 'Jeg er en Full-Stack-utvikler med fokus på HTML, CSS, JavaScript, React og implementering av nye teknologier som kan komplementere teknologistakken som brukes. Jeg kan kontaktes via e-post på heritier.kodehode@gmail.com.',
+      text2: 'For øyeblikket jobber jeg som front-end-utvikler hos Medretur siden 2022, hvor mine ansvarsområder inkluderer planlegging og avholdelse av møter, utvikling av UX/UI med React, og backend med Firebase og andre CMS-systemer.',
+      text3: 'Fra 2022 til 2022 jobbet jeg som front-end-utvikler for Jobloop, hvor jeg utviklet et planetarium med et team på tre. Jeg utviklet også kodebasen med Github-grenser, satt opp utviklingsmiljøet, og utviklet og vedlikeholdt en chatt-app.',
+      text4: 'Fra 2017 til 2022 jobbet jeg som frilans front-end-utvikler. Mine ansvarsområder inkluderte å fikse feil på front-end, implementere betalingsløsninger med Stripe, holde møter med front-end-teamet for å utforske svakheter i systemet, og inspisere kode for refaktorisering og optimalisering.',
     },
   ],
+
   projectText: [{ text: 'Besøk Side' }, { text: 'Se Koden' }],
   buttonText: [{ text: 'Flere prosjekter' }, { text: 'Kontakt Meg' }],
   sectionTitle: [
